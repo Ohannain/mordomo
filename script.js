26cac166e6e5b12c5da46bac4c95792f45cae829
Ohannain <finnhannon@online.de>
2024-10-31 14:44:23 +0100

    Added Mobile Menu Flyout

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ function changeWebsiteColourScheme(isDarkScheme) {
     }
 }
 
+function setFlyoutContainerHeight() {
+    const flyoutContainer = document.getElementById("header-mobile-flyout-container");
+    flyoutContainer.style.height = `${window.innerHeight}px`;
+}
+
 /**
  * Initiates the change of the websites colour scheme
  * 
@@ -21,12 +26,16 @@ function darkmodeBtnClicked() {
     if (document.getElementById("body").classList.contains("dark")) {
         changeWebsiteColourScheme(false);
         document.getElementById("header-darkmode-btn").children[0].textContent = "dark_mode";
+        document.getElementById("header-flyout-darkmode-btn").children[0].textContent = "dark_mode";
         document.getElementById("header-logo").src = "assets/logo/mordomo_dark_orange.svg";
+        document.getElementById("header-mobile-logo").src = "assets/logo/mordomo_dark_orange.svg";
         document.getElementById("footer-logo").src = "assets/logo/mordomo_dark_orange.svg";
     } else {
         changeWebsiteColourScheme(true);
         document.getElementById("header-darkmode-btn").children[0].textContent = "light_mode";
+        document.getElementById("header-flyout-darkmode-btn").children[0].textContent = "light_mode";
         document.getElementById("header-logo").src = "assets/logo/mordomo_light_orange.svg";
+        document.getElementById("header-mobile-logo").src = "assets/logo/mordomo_light_orange.svg";
         document.getElementById("footer-logo").src = "assets/logo/mordomo_light_orange.svg";
     }
 }
@@ -43,8 +52,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
     changeWebsiteColourScheme(userDarkScheme);
     document.getElementById("header-darkmode-btn").children[0].textContent = userDarkScheme ? "light_mode" : "dark_mode";
+    document.getElementById("header-flyout-darkmode-btn").children[0].textContent = userDarkScheme ? "light_mode" : "dark_mode";
     document.getElementById("footer-logo").src = userDarkScheme ? "assets/logo/mordomo_light_orange.svg" : "assets/logo/mordomo_dark_orange.svg";
     document.getElementById("header-logo").src = userDarkScheme ? "assets/logo/mordomo_light_orange.svg" : "assets/logo/mordomo_dark_orange.svg";
+    document.getElementById("header-mobile-logo").src = userDarkScheme ? "assets/logo/mordomo_light_orange.svg" : "assets/logo/mordomo_dark_orange.svg";
 }); 
 
 const colourSchemeChange = window.matchMedia("(prefers-color-scheme: dark)");
@@ -59,8 +70,10 @@ const colourSchemeChange = window.matchMedia("(prefers-color-scheme: dark)");
 colourSchemeChange.addEventListener("change", (event) => {
     changeWebsiteColourScheme(event.matches);
     document.getElementById("header-darkmode-btn").children[0].textContent = event.matches ? "light_mode" : "dark_mode";
-    document.getElementById("footer-logo").src = event.matches ? "assets/logo/mordomo_light_orange.svg" : "assets/logo/mordomo_dark_orange.svg";
+    document.getElementById("header-flyout-darkmode-btn").children[0].textContent = event.matches ? "light_mode" : "dark_mode";
     document.getElementById("header-logo").src = event.matches ? "assets/logo/mordomo_light_orange.svg" : "assets/logo/mordomo_dark_orange.svg";
+    document.getElementById("header-mobile-logo").src = event.matches ? "assets/logo/mordomo_light_orange.svg" : "assets/logo/mordomo_dark_orange.svg";
+    document.getElementById("footer-logo").src = event.matches ? "assets/logo/mordomo_light_orange.svg" : "assets/logo/mordomo_dark_orange.svg";
 });
 
 // document.addEventListener("click", (event) => {
@@ -76,7 +89,7 @@ colourSchemeChange.addEventListener("change", (event) => {
  * @returns {void}
  */
 document.addEventListener("click", (event) => {
-    if (event.target.id == "header-nav-products-btn") {
+    if (event.target.id == "header-nav-products-btn" && !document.getElementById("header-mobile-flyout-container").classList.contains("show")) {
         document.getElementById("header-dropdown-products").classList.toggle("show");
         document.getElementById("header-dropdown-inspiration").classList.remove("show");
         document.getElementById("header-dropdown-smarthome").classList.remove("show");
@@ -89,7 +102,7 @@ document.addEventListener("click", (event) => {
             document.getElementById("header-dropdown-container").classList.remove("show");
             document.body.style.overflow = "visible";
         }
-    } else if (event.target.id == "header-nav-inspiration-btn") {
+    } else if (event.target.id == "header-nav-inspiration-btn" && !document.getElementById("header-mobile-flyout-container").classList.contains("show")) {
         document.getElementById("header-dropdown-inspiration").classList.toggle("show");
         document.getElementById("header-dropdown-products").classList.remove("show");
         document.getElementById("header-dropdown-smarthome").classList.remove("show");
@@ -102,7 +115,7 @@ document.addEventListener("click", (event) => {
             document.getElementById("header-dropdown-container").classList.remove("show");
             document.body.style.overflow = "visible";
         }
-    } else if (event.target.id == "header-nav-smarthome-btn") {
+    } else if (event.target.id == "header-nav-smarthome-btn" && !document.getElementById("header-mobile-flyout-container").classList.contains("show")) {
         document.getElementById("header-dropdown-smarthome").classList.toggle("show");
         document.getElementById("header-dropdown-products").classList.remove("show");
         document.getElementById("header-dropdown-inspiration").classList.remove("show");
@@ -130,5 +143,37 @@ document.addEventListener("click", (event) => {
         window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
     } else if (event.target.id === "header-darkmode-btn") {
         darkmodeBtnClicked();
+    } else if (event.target.id === "header-flyout-darkmode-btn") {
+        darkmodeBtnClicked();
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const headerMobileMenuButton = document.getElementById("header-mobile-menu-btn");
+    const headerMobileMenuCloseButton = document.getElementById("header-mobile-flyout-close-btn");
+
+    headerMobileMenuButton.addEventListener("click", () => {
+        document.getElementById("header-mobile-flyout-container").classList.add("show");
+        if (window.innerWidth < 520) {
+            document.body.style.overflow = "hidden";
+        }
+        history.pushState(null, null, window.location.href);
+    });
+
+    headerMobileMenuCloseButton.addEventListener("click", () => {
+        document.getElementById("header-mobile-flyout-container").classList.remove("show");
+        document.body.style.overflow = "visible";
+    });
+});
+
+window.addEventListener("popstate", () => {
+    const headerMobileFlyoutContainer = document.getElementById("header-mobile-flyout-container");
+
+    if (headerMobileFlyoutContainer.classList.contains("show")) {
+        headerMobileFlyoutContainer.classList.remove("show");
+        document.body.style.overflow = "visible";
+    }
+});
+
+window.addEventListener("resize", setFlyoutContainerHeight);
+window.addEventListener("load", setFlyoutContainerHeight);
